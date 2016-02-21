96e14d7db801081ff41cdc4ecd8230346b3fef0e
Ahren Pfrehm <nathairtras@users.noreply.github.com>
2016-02-20 19:37:28 -0800

    Update setup.py
    words and numbers

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name='salesforce-python-toolkit',
     version='0.1.3',
-    description='A fork BayAreaCitizen\'s fork of http://code.google.com/p/salesforce-python-toolkit/',
+    description='A fork of BayAreaCitizen\'s fork of http://code.google.com/p/salesforce-python-toolkit/',
     url='http://github.com/nathairtras/salesforce-python-toolkit',
     packages=[
         'sforce',
     ],
 
     install_requires=[
-        'sf-suds==0.4.0',
+        'sf-suds==0.4.5',
     ],
 )
